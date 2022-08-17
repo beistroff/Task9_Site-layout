14ae7c58a7b1b089948968fd527fc075e3d832c6
beistroff <108692446+beistroff@users.noreply.github.com>
2022-08-17 13:22:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function upDate(background){
+    const btnBg = document.querySelectorAll(".PrimaryButton_primaryButton__fkDjg");
+    btnBg.forEach(PrimaryButton_primaryButton__fkDjg => {
+        PrimaryButton_primaryButton__fkDjg.remove();
+    });
+
+    const btnBgHover = document.querySelectorAll(".PrimaryButton_primaryButton__fkDjg");
+
+
+}
+
+function undo(){
+
+}
+
